82195c2784275c59164583775743c194d6d91ae0
fernandozamoraj <fernandozamoraj@hotmail.com>
2022-03-14 18:22:09 -0500

    fixed reported build errors for vercel

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,17 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Fernando's NextJS Playground</title>
+        <title>Fernando&apos;s NextJS Playground</title>
         <meta name="description" content="a first next js application" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Fernando's NextJS Playground
+          Fernando&apos;s NextJS Playground
         </h1>
         <p>
-          All code available at <a href="https://github.com/fernandozamoraj/nextjsplayground" target="_blank">https://github.com/fernandozamoraj/nextjsplayground</a>
+          All code available at <a href="https://github.com/fernandozamoraj/nextjsplayground" target="_blank" rel="noreferrer">https://github.com/fernandozamoraj/nextjsplayground</a>
         </p>
         <p className="bg-warning">
               This content requires a large (PC) screen for the best experience
@@ -38,17 +38,23 @@ export default function Home() {
           </a>
           </Link>
 
-          <Link href="/towersOfHanoi">
-            <a className={styles.card}>
-            <h2>Towers of Hanoi &rarr;</h2>
-            <p>Visual implementation of towers of hanoi</p>
-          </a>
-          </Link>
+          <div className="card" style={{width: '18rem;'}}>
+            <img className="card-img-top" src="/images/TowersThumbnailSmall.PNG" alt="towers of hanoi image"/>
+            <div className="card-body">
+              <h5 className="card-title">Visual Towers of Hanoi</h5>
+              <p className="card-text">Towers of hanoi</p>
+              
+              <Link href="/towersOfHanoi">
+                <a href="#" className="btn btn-primary">Go...</a>
+              </Link>
+            </div>
+          </div>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
+            rel="noreferrer"
           >
             <h2>Deploy &rarr;</h2>
             <p>
